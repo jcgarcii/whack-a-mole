251acaa89690e25e9701027295ca3a457e3a36b0
Jose Carlos Garcia <Carlos.cg895@live.com>
2022-09-17 15:23:14 -0500

    created initial screens

---
+++ app/src/main/java/com/example/cpre388/whack_a_mole/Activities/gameActivity.java
@@ -0,0 +1,16 @@
+package com.example.cpre388.whack_a_mole.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cpre388.whack_a_mole.R;
+
+public class gameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
