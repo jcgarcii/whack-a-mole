a5b4d29aa077f7e4f03bee1c57c37e9b48302b19
Jose Carlos Garcia <Carlos.cg895@live.com>
2022-09-17 13:56:55 -0500

    Added "authentication" screen pre-game

---
+++ app/src/main/java/com/example/cpre388/whack_a_mole/Activities/authActivity.java
@@ -0,0 +1,17 @@
+package com.example.cpre388.whack_a_mole.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cpre388.whack_a_mole.R;
+
+public class authActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+
+    }
+}
